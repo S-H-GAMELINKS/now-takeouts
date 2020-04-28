7aa13e821f80e40448af46ce44b9db39c89e17d0
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-29 02:12:02 +0900

    remove button in About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,6 @@ class About extends React.Component {
                 <p>
                     あなたの現在位置を使い、周辺一キロ以内のテイクアウト対応店舗を一覧で表示してくれるアプリです。
                 </p>
-                <p>
-                    <Button variant="primary">お店を探す</Button>
-                </p>
             </Jumbotron>
         );
     }
