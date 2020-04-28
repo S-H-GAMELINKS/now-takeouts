f1bcd0f8514d1d48ae51373681b85ecd1741dde6
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-29 08:43:48 +0900

    fix Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,53 +1,57 @@
 import React from 'react';
+import { isPointWithinRadius } from 'geolib';
 import { Row, Col, Container, Button, Card } from 'react-bootstrap';
 
+// import Place component
+import Place from './Place.jsx';
+
+// import Place data
+import places from '../data/places.json';
+
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: 0,
+            longitude: 0,
+            places: places
+        }
+        this.checkDistance = this.checkDistance.bind(this);
+    }
+
+    checkDistance(location, data) {
+        let result = isPointWithinRadius(location, data, 1000);
+        console.log(result);
+        return result;
+    }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            success => this.setState({ latitude: success.coords.latitude, longitude: success.coords.longitude })
+        );
+    }
+
     render() {
+        let placeData = this.state.places;
+        let result = false;
+
         return (
             <Container>
                 <div className="d-flex justify-content-center">
-                    <Button variant="primary">お店を探す</Button>
+                    <Button variant="primary">最寄りのお店一覧</Button>
                 </div>
                 <Row className="mt-5">
-                    <Col sm={12} md={4} lg={4} className="mt-2">
-                        <Card>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={12} md={4} lg={4} className="mt-2">
-                        <Card>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col sm={12} md={4} lg={4} className="mt-2">
-                        <Card>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    {placeData.map((data, i) => {
+                        result = this.checkDistance(this.state, { latitude: data.location.latitude, longitude: data.location.longitude });
+                        if (result) {
+                            return (
+                                <Col sm={12} md={4} lg={4} className="mt-2">
+                                    <Place info={data} />
+                                </Col>
+                            );
+                        }
+                    })}
                 </Row>
             </Container>
         );
