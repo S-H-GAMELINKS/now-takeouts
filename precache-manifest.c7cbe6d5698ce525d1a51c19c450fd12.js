1cf6b7053881de88e7100e0d1acda70db5e5dcd0
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-29 08:51:31 +0900

    Updates

---
+++ precache-manifest.c7cbe6d5698ce525d1a51c19c450fd12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e308d2c5cbf11f8a3f3a5cd07124e5",
+    "url": "/now-takeouts/index.html"
+  },
+  {
+    "revision": "68541c4a3c969facdeea",
+    "url": "/now-takeouts/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "86242a34a728f31c9df8",
+    "url": "/now-takeouts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68541c4a3c969facdeea",
+    "url": "/now-takeouts/static/js/2.fd03b464.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/now-takeouts/static/js/2.fd03b464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86242a34a728f31c9df8",
+    "url": "/now-takeouts/static/js/main.49bcaf9b.chunk.js"
+  },
+  {
+    "revision": "968cc94951dd5a7b31b4",
+    "url": "/now-takeouts/static/js/runtime-main.ee2a805b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/now-takeouts/static/media/logo.5d5d9eef.svg"
+  }
+]);
