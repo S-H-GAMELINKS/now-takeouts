a4a323408955d25c1f9d5ed2c7b42b3928c6ac59
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-04-29 09:04:28 +0900

    add github link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/NavBar';
+import { Navbar, Nav } from 'react-bootstrap';
 
 class Header extends React.Component {
     render() {
         return (
             <Navbar className="bg-primary">
                 <Navbar.Brand href="/" className="text-white">Hamada Now Takeout's</Navbar.Brand>
+                <Nav className="ml-auto">
+                    <Nav.Link href="https://github.com/Hamada-rb/now-takeouts" className="text-white">GitHub</Nav.Link>
+                </Nav>
             </Navbar>
         );
     }
